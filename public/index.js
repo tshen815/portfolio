dce77e1375f3f3d10f2757fe058c02c21ef52db5
Tony <tshen815@gmail.com>
2021-03-02 18:25:58 -0800

    use JS to add event listener for nav-toggle and all nav__links to implement opening and closing hamburger menu

---
+++ public/index.js
@@ -0,0 +1,13 @@
+const toggleNav = document.querySelector('.nav-toggle')
+const navLinks = document.querySelectorAll('.nav__link')
+
+toggleNav.addEventListener('click', () => {
+    document.body.classList.toggle("nav-open")
+})
+
+navLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        document.body.classList.remove('nav-open')
+    })
+})
+
